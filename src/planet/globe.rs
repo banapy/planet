40cd799cc578986b75c025801b176b073e9849ff
catnuko <346746061@qq.com>
2022-03-21 21:34:50 +0800

    添加天空和太阳的效果

--- src/planet/globe.rs
+++ src/planet/globe.rs
@@ -1,6 +1,7 @@
-use bevy::prelude::{Transform as BevyTransform};
 use bevy_utils::Uuid;
 use bevy::prelude::{PerspectiveCameraBundle};
+use bevy::prelude::*;
+use std::default::{Default};
 
 #[derive(Component)]
 pub struct UniqueId {
@@ -18,14 +19,27 @@ impl UniqueId {
 }
 
 #[derive(Component)]
-pub struct Transform {
-    ///local transform
-    pub transform: BevyTransform,
-    ///local transform
-    pub world_transform: BevyTransform,
+pub struct Rotable {
+    speed: f32,
 }
 
-impl Default for Transform {
+impl Default for Rotable {
+    fn default() -> Self {
+        Self {
+            speed: 0.0
+        }
+    }
+}
+
+#[derive(Component)]
+pub struct Transform3d {
+    ///local transform
+    pub transform: Transform,
+    ///local transform
+    pub world_transform: Transform,
+}
+
+impl Default for Transform3d {
     fn default() -> Self {
         Self {
             ..Default::default()
@@ -33,22 +47,23 @@ impl Default for Transform {
     }
 }
 
-
 #[derive(Bundle)]
 pub struct GlobeBundle {
-    pub name: NamedObj,
-    pub transform: Transform,
+    pub name: UniqueId,
+    pub transform: Transform3d,
+    pub rotable: Rotable,
 }
 
 impl GlobeBundle {
     pub fn new() -> Self {
-        const config: GlobeBundleConfig = Default::default();
+        let config: GlobeBundleConfig = Default::default();
         GlobeBundle::from_config(config)
     }
     pub fn from_config(config: GlobeBundleConfig) -> Self {
         GlobeBundle {
             name: UniqueId::new(config.name),
             transform: Default::default(),
+            rotable:Default::default(),
         }
     }
 }
@@ -63,4 +78,28 @@ impl Default for GlobeBundleConfig {
             name: String::from("planet_1")
         }
     }
+}
+
+pub fn setup_bundle(mut commands: Commands,
+                    mut meshes: ResMut<Assets<Mesh>>,
+                    mut materials: ResMut<Assets<StandardMaterial>>, ) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::UVSphere {
+            radius: 10.0,
+            ..Default::default()
+        })),
+        material: materials.add(Color::rgba(1.0, 0.0, 0.0, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
 }
--- src/planet/mod.rs
+++ src/planet/mod.rs
@@ -1,46 +1,72 @@
 pub mod globe;
 pub mod object;
 pub mod camera;
+pub mod atmosphere;
 
 use bevy::prelude::*;
 use std::default::{Default};
-
+use atmosphere::*;
 
 pub struct PlanetPlugin;
 
 impl Plugin for PlanetPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(Msaa { samples: 4 })
+            .insert_resource(AtmosphereMat::default()) // Default AtmosphereMat, we can edit it to simulate another planet
             .add_plugins(DefaultPlugins)
-            .add_startup_system(setup);
+            .add_startup_system(globe::setup_bundle)
+
+            .add_plugin(AtmospherePlugin {
+                dynamic: true,
+                sky_radius: 20.0,
+            })
+            .add_startup_system(setup_environment)
+            .add_system(daylight_cycle)
+
+            .add_startup_system(camera::setup_bundle)
+            .add_system(camera::camera_system)
+            .run();
     }
 }
 
-fn setup(
+#[derive(Component)]
+struct Sun;
+
+// We can edit the SkyMaterial resource and it will be updated automatically, as long as AtmospherePlugin.dynamic is true
+fn daylight_cycle(
+    mut sky_mat: ResMut<AtmosphereMat>,
+    mut query: Query<(&mut Transform, &mut DirectionalLight), With<Sun>>,
+    time: Res<Time>,
+) {
+    let mut pos = sky_mat.sun_position;
+    let t = time.time_since_startup().as_millis() as f32 / 1500.0;
+    pos.y = t.sin();
+    pos.z = t.cos();
+    sky_mat.sun_position = pos;
+
+    if let Some((mut light_trans, mut directional)) = query.single_mut().into() {
+        light_trans.rotation = Quat::from_rotation_x(-pos.y.atan2(pos.z));
+        directional.illuminance = t.sin().max(0.0).powf(2.0) * 100000.0;
+    }
+}
+
+// Simple environment
+fn setup_environment(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // cube
+    // Our Sun
+    commands
+        .spawn_bundle(DirectionalLightBundle {
+            ..Default::default()
+        })
+        .insert(Sun); // Marks the light as Sun
+
+    // Simple cube just for reference
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..Default::default()
-    });
-    // light
-    commands.spawn_bundle(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..Default::default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..Default::default()
-    });
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        material: materials.add(StandardMaterial::from(Color::rgb(1.0, 0.0, 0.5))),
         ..Default::default()
     });
 }
